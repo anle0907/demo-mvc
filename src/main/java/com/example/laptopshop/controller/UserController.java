98a021901c90fad5195260c45742792f29858c76
AnLe <anle.noctis@gmail.com>
2026-01-27 10:37:50 +0700

    repository service inject

--- src/main/java/com/example/laptopshop/controller/UserController.java
+++ src/main/java/com/example/laptopshop/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.laptopshop.domain.User;
+
 import com.example.laptopshop.service.UserService;
 import org.springframework.ui.Model;
 
@@ -13,32 +14,35 @@ import org.springframework.ui.Model;
 public class UserController {
 
     // UserService là dependency được inject vào Controller
-    private UserService userService;
+    private final UserService userService;
 
     // Inject UserService thông qua constructor
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/") // url web
     public String getHomePage(Model model) {
 
-        String test = this.userService.handleHello();
-        model.addAttribute("anle", test);
+        model.addAttribute("anle", "test");
         model.addAttribute("vuavanhdai", "from controller with model");
         return "hello";
     }
 
     @RequestMapping("/admin/user") // url web //method GET
     public String getInforPage(Model model) {
-        model.addAttribute("newUser", new User()); // Oject class User o Domain
+        model.addAttribute("newUser", new User()); // Object class User o Domain
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User an) { // ModelAttribute la thuoc tinh tu
-                                                                                    // form ben view
+    public String createUserPage(Model model, @ModelAttribute("newUser") User an) {
+        // @ModelAttribute dùng để bind dữ liệu từ form (view)
+        // vào object User (biến an) trong Controller
+
         System.out.println("Run here" + an);
+        this.userService.handleSaveUser(an); // method define o service
         return "hello";
     }
 }
--- src/main/java/com/example/laptopshop/service/UserService.java
+++ src/main/java/com/example/laptopshop/service/UserService.java
@@ -2,13 +2,28 @@ package com.example.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.laptopshop.domain.User;
+import com.example.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    // constructor di repository
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello() {
         return "Hello from service";
     }
 
     public String showInfo() {
         return "user";
+    }
+
+    public User handleSaveUser(User user) {
+        User an = this.userRepository.save(user);
+        return an;
     }
 }
